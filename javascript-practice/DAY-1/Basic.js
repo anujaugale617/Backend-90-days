cc5017d2258b273365f19e21882e86ddd031965d
Anuja Vitthal Ugale <ugaleanuja0617@gmail.com>
2026-07-14 18:22:39 +0530

    Add files via upload

---
+++ javascript-practice/DAY-1/Basic.js
@@ -0,0 +1,21 @@
+console.log("Hello, World!");
+a= "Anuja"
+console.log(a)
+// let 8harry=7 must begin with letters symbols not my number
+let b= "harry"
+const author="Riya"
+// let author =5 throws error because constant cant be change
+b=4
+let c=null
+let d=undefined
+{
+    let b ="Anu"
+    console.log(b)
+}
+console.log(b)
+var l="siya"
+{
+    var l="Diya"
+    console.log(l)
+}
+console.log(l)
---
+++ javascript-practice/DAY-1/datatype.js
@@ -0,0 +1,28 @@
+// primitive datatypes
+let a=null;
+let b=345;
+let c=true;
+let d=BigInt("456")
+let e="Ram"
+let f=Symbol("I am a Symbol")
+let g =undefined
+console.log(a,b,c,d,e,f,g)
+console.log(typeof d)
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+console.log(typeof e)
+console.log(typeof f)
+console.log(typeof g)
+
+//Non primitive = objects
+const item = {
+    "anuja":true,
+    "vedu":false,
+    "siya":67,
+    "ram":undefined
+}
+console.log(item["anuja"])
+console.log(item["anma"])
+console.log(item["siya"])
+
