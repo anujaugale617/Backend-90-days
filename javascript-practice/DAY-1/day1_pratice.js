420c9c6f29a80c10e2bf69c601eaa166428d756b
Anuja Vitthal Ugale <ugaleanuja0617@gmail.com>
2026-07-18 00:12:33 +0530

    Add files via upload

---
+++ javascript-practice/DAY-1/day1_pratice.js
@@ -0,0 +1,48 @@
+// console.log("Hello Jvascript")
+// let name="PIu"
+// console.log(name)
+// const country ="India"
+// console.log(country)
+// let myname="Anuja"
+// let age=20;
+// let city="Nashik"
+// console.log(myname,age,city)
+// let score=10
+// score=20
+// console.log(score)
+// var b=0
+// let d=null
+// const n=23
+ let name="radha"
+ let b= 90;
+ let c=true
+ let d=undefined
+ let e=null
+ let f=BigInt("890")
+ let u=Symbol("my name is radha")
+
+ console.log( name , b , c , d, e, f, u)
+ let name1="Rahul"
+ console.log(typeof name1)
+ let age=20;
+ console.log(typeof age)
+ let isStudent=true
+ console.log(typeof isStudent)
+
+
+var first="Anuja"
+var last="ugale "
+console.log(first + last)
+var ab=78
+var bc = 89
+console.log(first+ bc)
+console.log(typeof (first+bc))
+
+const u1 = {
+    name:"suti",
+    section:5,
+    isteacher: false
+}
+u1['friend'] ="shubham"
+u1['ROLLNO'] =26  
+console.log(u1)
